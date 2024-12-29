ecc5ab57d0883fd40928542461a46a027a9b719b
Gayathri_Kaushalya <150233989+gayathri-dunuthilaka@users.noreply.github.com>
2024-12-29 17:37:34 +0530

    POST book with empty title test case added.

--- api-testing/src/tests/features/postBook.feature
+++ api-testing/src/tests/features/postBook.feature
@@ -36,3 +36,13 @@ Scenario: Attempt to create two different books with the same author but differe
     When I send a POST request to the "books" endpoint with two different titles and the same author
     Then the response status of POST should be either 201 or 208
     And the response should contain the details of both books
+
+Scenario: Verify the response of the POST /api/books API as admin with empty title 
+  Given I am an authenticated POST admin API client
+  When I send a POST request to the "books" endpoint with empty title
+  Then the response status of POST should be 400
+
+Scenario: Verify the response of the POST /api/books API as user with empty title 
+  Given I am an authenticated POST user API client
+  When I send a POST request to the "books" endpoint with empty title
+  Then the response status of POST should be 400
--- api-testing/src/tests/steps/postBook.steps.ts
+++ api-testing/src/tests/steps/postBook.steps.ts
@@ -4,6 +4,7 @@ import { BooksPage } from '../../pageObjects/BooksPage';
 import { credentials } from '../../config';
 import {  validateBookCreationResponse,validateBookCreationResponseDifferentTiltle,validateBookCreationResponseEmpty } from '../../requests/BookCreation';
 import {  validateResponseTwoStatus } from '../../requests/ValidateResponseStatus'; 
+import { expect } from 'playwright/test';
 
 let booksPage: BooksPage;
 let response: any;
@@ -55,6 +56,19 @@ When('I send a POST request to the "books" endpoint with two different titles an
     await booksPage.createBook(bookDetails2);
 });
 
+When('I send a POST request to the {string} endpoint with empty title', async function (endpoint: string) {
+    if (endpoint === 'books') {
+        const bookDetails = {
+            title: '', // Empty title
+            author: 'Some Author'
+        };
+        response = await booksPage.createBook(bookDetails);
+    } else {
+        throw new Error(`Unknown endpoint: ${endpoint}`);
+    }
+});
+
+
 
 Then('the response status of POST should be either {int} or {int}', (status1: number, status2: number) => {
     validateResponseTwoStatus(response, status1, status2);
@@ -70,5 +84,9 @@ Then('the response should contain the created book details with empty title and 
 
 Then('the response should contain the details of both books', async () => {
     await validateBookCreationResponseDifferentTiltle(response);
+});
+
+Then('the response status of POST should be {int}', (status: number) => {
+    expect(response.status()).toBe(status);
 });
 
