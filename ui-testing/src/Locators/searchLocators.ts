84e51b92b108d485f5b078967755ac13a8d4559e
Rashini Nethmini <s.a.r.nethmini@gmail.com>
2025-01-03 10:45:04 +0530

    Search a product by name UI testcase added

---
+++ ui-testing/src/Locators/searchLocators.ts
@@ -0,0 +1,6 @@
+export const searchLocators = {
+    searchInput: 'input#search-query',
+    searchButton: 'button[data-test="search-submit"]',
+    productNameElement: 'h5[data-test="product-name"]',
+  };
+  
