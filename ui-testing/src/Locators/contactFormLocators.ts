113029cb09355d048a0274e400cca7b35de20c56
Gayathri_Kaushalya <150233989+gayathri-dunuthilaka@users.noreply.github.com>
2025-01-03 17:05:01 +0530

    Filling contact form with valid details as a non logged user UI testcase added

--- ui-testing/src/Locators/contactFormLocators.ts
+++ ui-testing/src/Locators/contactFormLocators.ts
@@ -7,5 +7,13 @@ export const contactFormLocators = {
     messageTextarea: '[data-test="message"]',
     submitButton: '[data-test="contact-submit"]',
     successAlert: 'div.alert.alert-success',
+
+      
+  errorMessages: {
+    lastNameRequired: 'div:has-text("Last name is required")',
+    invalidEmail: 'div:has-text("Email format is invalid")',
+    subjectRequired: 'div:has-text("Subject is required")',
+    messageTooShort: 'div:has-text("Message must be minimal 50 characters")',
+  },
+
   };
-  
--- ui-testing/src/tests/features/contactForm.feature
+++ ui-testing/src/tests/features/contactForm.feature
@@ -7,5 +7,12 @@ Feature: Contact form function
     And I press submit
     Then the form must be submitted
 
+  Scenario: Filling contact form with invalid details as a non logged user
+    
+    Given I'm in the contact form page
+    When I add invalid details
+    And I press submit
+    Then there has to be error messages
+
     
 
